7d61577b370ff84fc85d569084e71d1c3ca52b8a
Anuj Arora <anujarora88@gmail.com>
2013-02-26 09:18:46 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SkybullsRails::Application.config.secret_token = '22c281c25004257d9c99069cf79c0e3df74eafa64bd60aa386b55abed711be4d8b5a75697448ef5a552aae9bb01c76597ced91f26df05363bbd048a1820bffeb'
