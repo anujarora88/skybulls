12850e32659e66a8cea04307e82a3d34376c3665
Anuj Arora <anujarora88@gmail.com>
2013-03-27 18:11:04 -0300

    Minor changes

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -5,17 +5,17 @@ class AuthenticationsController < Devise::OmniauthCallbacksController
   def google
     auth = request.env["omniauth.auth"]
     data = auth.info
-    parse_auth_callback(auth, data["email"])
+    parse_auth_callback(auth, data["email"], request.env["omniauth.params"])
   end
 
   def facebook
     auth = request.env["omniauth.auth"]
-    parse_auth_callback(auth, auth['extra']['raw_info']['email'])
+    parse_auth_callback(auth, auth['extra']['raw_info']['email'], request.env["omniauth.params"])
   end
 
   private
 
-    def parse_auth_callback(auth, email)
+    def parse_auth_callback(auth, email, omniauth_params)
 
       # Try to find authentication first
       authentication = Authentication.find_by_provider_and_uid(auth['provider'], auth['uid'])
@@ -31,17 +31,23 @@ class AuthenticationsController < Devise::OmniauthCallbacksController
         flash[:notice] = "Signed in successfully."
         sign_in_and_redirect(:user, authentication.user)
       else
-        # Authentication not found, thus a new user.
-        user = User.where(:email => email).first || User.new(:password => Devise.friendly_token[0,20])
-        user.apply_omniauth(auth, email)
-        user.skip_confirmation!
-        if user.save(:validate => false)
-          flash[:notice] = "Account created and signed in successfully."
-          sign_in_and_redirect(:user, user)
+        # Authentication not found, thus a new user.  TODO maybe show a message
+        if omniauth_params.with_indifferent_access[:signup]
+          user = User.where(:email => email).first || User.new(:password => Devise.friendly_token[0,20])
+          user.apply_omniauth(auth, email)
+          user.skip_confirmation!
+          if user.save(:validate => false)
+            flash[:notice] = "Account created and signed in successfully."
+            sign_in_and_redirect(:user, user)
+          else
+            flash[:error] = "Error while creating a user account. Please try again."
+            redirect_to new_user_session_path
+          end
         else
-          flash[:error] = "Error while creating a user account. Please try again."
-          redirect_to root_url
+          flash[:error] = "User not found!!."
+          redirect_to new_user_session_path
         end
+
       end
     end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,48 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130325203532) do
+ActiveRecord::Schema.define(:version => 20130326120723) do
+
+  create_table "admin_users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
+  add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
     t.string   "provider"
     t.string   "uid"
     t.string   "token"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "leagues", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "category"
+    t.string   "market"
+    t.string   "algo_name"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "user_leagues", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "league_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
