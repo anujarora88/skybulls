d2bad7c7c9f26f6838146ae8c74287ebcec88daa
Anuj Arora <anujarora88@gmail.com>
2013-04-09 13:30:56 -0300

    Minor change

--- db/migrate/20130402162135_create_user_transactions.rb
+++ db/migrate/20130402162135_create_user_transactions.rb
@@ -3,7 +3,6 @@ class CreateUserTransactions < ActiveRecord::Migration
     create_table :user_transactions do |t|
       t.integer :payment_method_id, null:false
 
-      t.foreign_key :user_accounts, :column => :account_id
       t.foreign_key :user_payment_methods, :column => :payment_method_id
       t.money :amount, currency: { present: false }
       t.string :type, null: false
