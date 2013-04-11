24b675ca99d25701221e86b742256cf6918838da
prashant <pal.prashant31@gmail.com>
2013-04-12 01:16:09 +0530

    Create league updated code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Exchange.create(:name => 'NasDaq',
+               :logo_url => '/images/wd4d.jpg',
+               :starts_at =>   Time.now,
+               :ends_at =>  Time.now+2,
+               :symbol => 'NASDAQ')
+
+Exchange.create(:name => 'Bombay Stock Exchange',
+                :logo_url => '/images/bse.jpg',
+                :starts_at =>   Time.now,
+                :ends_at =>  Time.now+3,
+                :symbol => 'BSE')
+
+Exchange.create(:name => 'Ney York Stock Exchange',
+                :logo_url => '/images/nyse.jpg',
+                :starts_at =>   Time.now,
+                :ends_at =>  Time.now+4,
+                :symbol => 'NYSE')
