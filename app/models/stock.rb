57c05de6276319300328747a4192bfc182e2b479
Anuj Arora <anujarora88@gmail.com>
2013-04-18 19:13:24 -0300

    Added money support to stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,5 +6,9 @@ class Stock < ActiveRecord::Base
   has_many :trades
   has_many :bids
 
+  monetize :latest_price_cents, :allow_nil => false, :numericality => {
+      :greater_than_or_equal_to => 0
+  }
+
 
 end
