29478f87fca58e31889f2245c4d12f11caea96ed
Anuj Arora <anujarora88@gmail.com>
2013-05-04 13:41:09 -0400

    Staging config

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,9 +4,19 @@ SkybullsRails::Application.configure do
   config.paypal_merchant_account_email = "anuj@skybulls.com"
   config.paypal_preapproval_key_url = "https://www.sandbox.paypal.com/webscr&cmd=_ap-preapproval&preapprovalkey="
 
+  config.action_mailer.raise_delivery_errors = true
+
+  # Print deprecation notices to the Rails logger
+  config.active_support.deprecation = :log
+
+  # Only use best-standards-support built into browsers
+  config.action_dispatch.best_standards_support = :builtin
+
+  # Raise exception on mass assignment protection for Active Record models
+  config.active_record.mass_assignment_sanitizer = :strict
+
 
   # Expands the lines which load the assets
-  config.assets.debug = true
   config.action_mailer.default_url_options = { :host => 'www.skybulls.com' }
   config.nodeapp_url = "http://node.skybulls.com"
 end
